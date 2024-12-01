295600580ba6e09870838bc15cd3bdd53836d85f
unknown <tiago.rafael019@gmail.com>
2024-12-01 00:30:16 -0300

    first commit

--- src/app/sigin/page.tsx
+++ src/app/sigin/page.tsx
@@ -1,21 +1,24 @@
 // Dados
-import companies from '../../data/settings/companies.json'
+import companies from '../../data/settings/companies.json';
 
 // Componentes
 import { NotFound } from '@/components/notFound';
 
-export default function SigIn({ searchParams }: { searchParams: { [key: string]: string | string[] } }) {
+// Tipos fornecidos pelo Next.js
+import { PageProps } from './../../../.next/types/app/(landingPage)/help/page';
+
+export default function SigIn({
+    searchParams,
+}: PageProps & { searchParams: Record<string, string | string[]> }) {
     const company = searchParams.company as string;
 
-    const foundCompany = companies.find((item: any) => item.name.toLowerCase() === company?.toLowerCase())
+    const foundCompany = companies.find(
+        (item: any) => item.name.toLowerCase() === company?.toLowerCase()
+    );
 
     if (foundCompany) {
-        return (
-            <p>encontrou</p>
-        )
+        return <p>encontrou</p>;
     } else {
-        return (
-            <NotFound />
-        )
+        return <NotFound />;
     }
 }
